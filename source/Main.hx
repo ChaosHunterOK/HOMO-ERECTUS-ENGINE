1eff288ebfce1bae73c9e1226dff52c4519984e6
CopiluCuSarmale <alexneculai2009@gmail.com>
2026-05-10 14:48:04 +0300

    oops

--- source/Main.hx
+++ source/Main.hx
@@ -109,7 +109,7 @@ class Main extends Sprite
             if (options.fpsCap != null) framerate = options.fpsCap;
             if (options.showHaxeSplash != null) skipSplash = !options.showHaxeSplash;
         }
-        #if (haxe <= "4.11.0")
+        #if (haxe >= "4.11.0")
         var game = new FlxGame(gameWidth, gameHeight, initialState, 1, framerate, framerate, skipSplash, false);
         #else
         var game = new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, false);
